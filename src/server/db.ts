7b0950cb62fb79a4c07055c645bbf6b5b42cc556
realmers <danhoalexander123@gmail.com>
2026-01-26 20:07:45 +0100

    init

---
+++ src/server/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "@/env";
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
+const createPrismaClient = () =>
+  new PrismaClient({
+    adapter,
+    log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
