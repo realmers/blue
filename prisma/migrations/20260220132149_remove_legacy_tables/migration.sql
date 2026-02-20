4b5d6f0102249bfb1c4e420e3d5a057265581191
realmers <71337913+realmers@users.noreply.github.com>
2026-02-20 20:00:20 +0100

    Updated readme, removed legacy db tables, uploaded development sql dump (#38)

---
+++ prisma/migrations/20260220132149_remove_legacy_tables/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `legacy_availability` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `legacy_competence` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `legacy_competence_profile` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `legacy_person` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `legacy_role` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "legacy_availability";
+
+-- DropTable
+DROP TABLE "legacy_competence";
+
+-- DropTable
+DROP TABLE "legacy_competence_profile";
+
+-- DropTable
+DROP TABLE "legacy_person";
+
+-- DropTable
+DROP TABLE "legacy_role";
